109f0e251d10322dec7e1e0a04b543d080c9a3a0
Albert <ron468896@gmail.com>
2023-01-06 23:26:29 +0000

    Dish modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -174,6 +174,7 @@ const Home = () => {
   };
   const handleDishModalClose = async () => {
     setSelectedDish({});
+    document.body.style.overflow = "initial";
     setOpenDishModal(false);
   };
 
