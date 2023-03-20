a0c221737a7614d9f5a1222c166110e63f025c5f
Albert <ron468896@gmail.com>
2023-03-20 16:25:57 +0000

    Start order processing

--- src/components/Routes/AdminRoute.js
+++ src/components/Routes/AdminRoute.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import io from "socket.io-client";
 
+import Ably from "ably/promises";
+
 import LoadingToRedirect from "./LoadingToRedirect";
 import { currentAdmin } from "../../serverFunctions/auth";
 import { toast } from "react-toastify";
@@ -31,27 +33,42 @@ const AdminRoute = (props) => {
   }, [user]);
 
   useEffect(() => {
-    const socket = io("https://wudalounge-server.vercel.app/");
+    // const socket = io("https://wudalounge-server.vercel.app/");
 
-    socket.on("connect", () => {
-      console.log("Socket connected: ", socket.id);
-    });
+    // socket.on("connect", () => {
+    //   console.log("Socket connected: ", socket.id);
+    // });
 
-    socket.on("newOrder", (data) => {
-      // window.alert("New order received=Web Socket: ", data);
-      toast.success("new order received");
-      // getChartData();
-      // getBriefs();
-    });
+    // socket.on("newOrder", (data) => {
+    //   // window.alert("New order received=Web Socket: ", data);
+    //   toast.success("new order received");
+    //   // getChartData();
+    //   // getBriefs();
+    // });
 
-    socket.on("disconnect", () => {
-      console.log("Socket disconnected");
+    // socket.on("disconnect", () => {
+    //   console.log("Socket disconnected");
+    // });
+
+    // return () => {
+    //   socket.disconnect();
+    // };
+    const ably = new Ably.Realtime(
+      "zf8B2g.48jGgQ:srWXyb4--QGlLCKAV3RlVsoJ6D-tHOxxtU4d7c3xXRg"
+    );
+    const channel = ably.channels.get("newOrder");
+
+    channel.subscribe("newOrder", (data) => {
+      console.log("New message received:", data);
+      toast.success(`New order received from `);
+      // Do something with the new message here
     });
 
     return () => {
-      socket.disconnect();
+      channel.unsubscribe();
+      ably.close();
     };
-  });
+  }, []);
 
   return ok ? (
     <Box>
