9327ebd25347160f31b8565530d6a10dfb9a7160
Albert <ron468896@gmail.com>
2023-04-27 09:43:09 +0000

    Start papaye customization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -468,7 +468,7 @@ const Home = (props) => {
           p: 2,
           // cursor: "pointer",
           borderRadius: "12px",
-          background: "rgba(255,255,255, 1)",
+          background: "rgba(255,255,255, 0.6)",
 
           boxShadow: "0 4px 30px rgba(0, 0, 0, 0.2)",
           webkitBackdropFilter: "blur(5px)",
